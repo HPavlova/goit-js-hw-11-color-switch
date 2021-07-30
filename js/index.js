3693f38f07a0a6a8e261edb87a0b7ddeca176a4c
HPavlova <77845327+HPavlova@users.noreply.github.com>
2021-07-30 19:37:51 +0300

    добавляет index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,37 @@
 const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
+const backgroundChange_DELAY = 1000;
+let timeoutId = null;
+
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+
+// console.log(refs);
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+refs.startBtn.addEventListener('click', onBackgroundChangeClick);
+refs.stopBtn.addEventListener('click', onBackgroundStopClick);
+
+function onBackgroundStopClick() {
+  stopBackground();
+  clearTimeout(timeoutId);
+}
+
+function onBackgroundChangeClick() {
+  refs.startBtn.disabled = true;
+
+  timeoutId = setInterval(() => {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, backgroundChange_DELAY);
+}
+
+function stopBackground() {
+  // refs.body.style.backgroundColor = '';
+  refs.startBtn.disabled = false;
+}
